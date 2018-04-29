01735c3c761653ab28852337ac66b4efa3e79937
devwillee <devwillee@gmail.com>
2018-04-29 16:47:47 +0900

    apply mvp architecture

--- app/src/main/java/io/github/xeyez/architecture/view/MainActivity.kt
+++ app/src/main/java/io/github/xeyez/architecture/view/MainActivity.kt
@@ -1,16 +1,40 @@
 package io.github.xeyez.architecture.view
 
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
+import android.view.View
 import io.github.xeyez.architecture.R
+import io.github.xeyez.architecture.presenter.MainPresenter
+import io.github.xeyez.architecture.presenter.base.MainMVPContract
+import kotlinx.android.synthetic.main.activity_main.*
 import org.androidannotations.annotations.AfterViews
+import org.androidannotations.annotations.Bean
+import org.androidannotations.annotations.Click
 import org.androidannotations.annotations.EActivity
 
 @EActivity(R.layout.activity_main)
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MainMVPContract.View {
+
+    @Bean
+    lateinit var presenter: MainPresenter
 
     @AfterViews
     fun onAfterViews() {
-        Log.d("###DEBUG", "afterViews!")
+        presenter.setView(this)
+    }
+
+    @Click(R.id.btn_count)
+    fun onClick(v: View) {
+        var tag = if (tv_hello.tag == null) {
+            0
+        } else {
+            tv_hello.tag.toString().toInt()
+        }
+
+        presenter.onRequestOperation(tag)
+    }
+
+    override fun onResultOperation(result: Int) {
+        tv_hello.tag = result
+        tv_hello.text = tv_hello.tag.toString()
     }
 }
